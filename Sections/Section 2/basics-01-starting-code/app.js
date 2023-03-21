489c8c88b7892470cf6a8bd6a2266e65e5830220
steffengroenandersen <steffengroenandersen@gmail.com>
2023-03-21 18:14:22 +0100

    Added Section 1 + 2

---
+++ Sections/Section 2/basics-01-starting-code/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: '<h2>Master vue and build amazing apps!</h2>',
+      vueLink: "https://vuejs.org/",
+    };
+  },
+
+  methods: {
+    outputGoal(){
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+            return this.courseGoalA;
+        } else {
+            return this.courseGoalB;
+        }
+    }
+  },
+});
+
+app.mount("#user-goal");
