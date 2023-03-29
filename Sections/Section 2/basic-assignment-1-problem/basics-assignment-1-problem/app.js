4305c855209aed78711e07b175202797a03ab6e8
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-03-29 07:45:30 +0200

    Finished Assignment One.

--- Sections/Section 2/basic-assignment-1-problem/basics-assignment-1-problem/app.js
+++ Sections/Section 2/basic-assignment-1-problem/basics-assignment-1-problem/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       yourName: "Steffen Grøn Andersen",
       yourAge: 31,
+      vueLink: "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg",
     };
   },
 
@@ -10,6 +11,11 @@ const app = Vue.createApp({
     outputAge() {
       return this.yourAge + 5;
     },
+    
+    outputRandomNumber() {
+      return Math.random();
+    },
+
   },
 });
 
