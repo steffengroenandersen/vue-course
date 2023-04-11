86075509043f344479db208c8bc2f1fbcacfddc2
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-04-11 21:48:22 +0200

    Added and finished Section 5

---
+++ Sections/Section 5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+// ....
+
+let message = 'Hello!';
+
+let longMessage = message + ' World!'
+
+message = 'Hello!!!!';
